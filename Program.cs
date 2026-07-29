817f43ebd11197b44b449977e89c10f439d63813
benj4865 <bena380dk@gmail.com>
2026-07-29 22:30:20 +0200

    Refactor PipeClient to support multiple connections and update message handling

--- Program.cs
+++ Program.cs
@@ -18,6 +18,10 @@ Console.WriteLine("Press enter or escape to exit.");
 Directory.CreateDirectory(DEBUG_FRAME_FOLDER);
 Cv2.NamedWindow(WINDOW_NAME, WindowFlags.AutoSize);
 
+var directClient = new PipeClient();
+directClient.MessageReceived += ReceivedMessageHandler;
+directClient.Connect(MODULE_NAME, "VideoStreamerPipe");
+
 // Create a task that listens for console into for closing the window.
 // Its not pretty, but it works
 var exitTask = Task.Run(Console.ReadLine);
@@ -56,11 +60,11 @@ lock (frameLock)
 Cv2.DestroyAllWindows();
 
 
-void ReceivedMessageHandler(object? eventSender, TartaMessage tartaMessage)
+void ReceivedMessageHandler(object? eventSender, PipeMessageEventArgs message)
 {
-    if (tartaMessage.Type == "message" && tartaMessage.SubCategory == "videoframe")
+    if (message.TartaMessage.Type == "message" && message.TartaMessage.SubCategory == "videoframe")
     {
-        var encodedFrame = Convert.FromBase64String(tartaMessage.Payload);
+        var encodedFrame = Convert.FromBase64String(message.TartaMessage.Payload);
         using var frame = Cv2.ImDecode(encodedFrame, ImreadModes.Color);
 
         if (frame.Empty())
@@ -68,9 +72,6 @@ void ReceivedMessageHandler(object? eventSender, TartaMessage tartaMessage)
             Console.WriteLine("Received an empty or invalid JPEG frame.");
             return;
         }
-
-        //var debugFramePath = Path.Combine(DEBUG_FRAME_FOLDER, $"frame_{DateTime.Now:yyyyMMdd_HHmmss_fff}.jpg");
-        //Cv2.ImWrite(debugFramePath, frame);
 
         lock (frameLock)
         {
